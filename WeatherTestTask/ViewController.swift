5302c1cb908efac3c07fbafe1be844837cc8c8fa
MaxPasuta <pasutmaks@gmail.com>
2022-01-31 20:12:49 +0700

    Initial Commit

---
+++ WeatherTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherTestTask
+//
+//  Created by Максим Пасюта on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
